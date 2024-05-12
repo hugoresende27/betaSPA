b92adb81bd21d023d785ea032c274cc1e74b0231
hugoresende <hugo.resende@iservices.pt>
2024-05-12 10:20:40 +0100

    debugbar and database

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,16 @@ import MainLayout from './Layouts/MainLayout.vue';
 
 createInertiaApp({
   resolve: async (name) => {
-    const pages = import.meta.glob('./Pages/**/*.vue')
-    const page = (await pages[`./Pages/${name}.vue`]())
+    const pages = import.meta.glob('./Pages/**/*.vue') // https://vitejs.dev/guide/features.html#glob-import
+    const page = await pages[`./Pages/${name}.vue`]()
     page.default.layout = page.default.layout || MainLayout
 
     return page
     
   },
+
+  //function () {}
+  // {el : xxx, App : xxx, props : xxx}
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
