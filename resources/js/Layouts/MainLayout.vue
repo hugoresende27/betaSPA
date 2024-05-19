9f663cc3e662a1c0067b382c211880e719f55a6f
hugoresende <hugo.resende@iservices.pt>
2024-05-19 19:27:41 +0100

    tailwind end

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -17,7 +17,7 @@
                 </div>
                 <div>
                     <Link :href="route('listing.create')"
-                            class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md"
+                            class="btn-primary"
                             >+ New Listing</Link>
                 </div>
             
