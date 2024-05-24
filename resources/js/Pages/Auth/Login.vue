3ac4e9f271fd84db970c977e588710fff4868987
hugoresende <hugo.resende@iservices.pt>
2024-05-24 20:52:54 +0100

    auth lesson

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="login">
+
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email">E-mail (username)</label>
+                <input type="text" id="email" class="input" v-model="form.email" />
+
+                <div class="input-error">
+                    Potential errors
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <label for="pass_secret">Password</label>
+                <input type="password" id="pass_secret" class="input" v-model="form.pass_secret" />
+            </div>
+
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Login</button>
+            </div>
+
+
+        </div>
+
+
+    </form>
+</template>
+
+
+<script setup>
+
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm( {
+    email: null,
+    password: null,
+})
+
+const login = () => form.post(route('login.store'));
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,8 @@ Route::get('/', [IndexController::class, 'index'])->name('index');
 Route::get('/hello', [IndexController::class, 'show'])->name('hello');
 
 Route::resource('/listing', ListingController::class);//->except(['destroy']);//->only(['index','show','create','store']);
+
+
+Route::get('login',[AuthController::class, 'create'] )->name('login');
+Route::post('login',[AuthController::class, 'store'] )->name('login.store');
+Route::delete('logout',[AuthController::class, 'destroy'] )->name('logout');
